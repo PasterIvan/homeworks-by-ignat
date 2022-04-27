a9acdd6829145e00e14ec9ab49ca3273284a989f
Ivan Pasternak <paster-ivan@yandex.by>
2022-04-27 14:26:17 +0300

    HW4

--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -19,8 +19,13 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
         ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
+
+
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // сделайте так чтоб работал onChange и onChangeChecked
+        onChange
+        && onChange(e)
+
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)  // сделайте так чтоб работал onChange и onChangeChecked
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
