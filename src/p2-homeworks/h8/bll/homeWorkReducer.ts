e925706173cd832a00d7c74d9ab236a724f05874
Ivan <paster-ivan@yandex.by>
2022-05-31 23:44:29 +0300

    HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,8 +1,23 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export type sortAT = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+export type checkAT = {
+    type: 'check'
+    payload:  number
+}
+
+export type CounterReducerAT = sortAT | checkAT
+
+export const homeWorkReducer = (state:  Array<UserType>, action: CounterReducerAT): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return action.payload === 'up'
+                ? [...state.sort((a,b)=>(a.name < b.name ? -1:1))]
+                : [...state.sort((a,b)=>(a.name > b.name ? -1:1))]
         }
         case 'check': {
             // need to fix
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
+import {UserType} from "../../HW8";
 
-let initialState: any[] // need to fix any
+let initialState: Array<UserType> // need to fix any
 
 beforeEach(() => {
     initialState = [
@@ -18,15 +19,21 @@ test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
     console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0].name).toBe('Александр')
+    expect(newState[1].name).toBe('Виктор')
+    expect(newState[2].age).toBe(40)
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
-
+    expect(newState[0].name).toBe('Кот')
+    expect(newState[1].name).toBe('Коля')
+    expect(newState[2].age).toBe(55)
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
-
+    expect(newState[0].name).toBe('Александр')
+    expect(newState.length).toBe(4)
+    expect(newState.find((u: UserType) => u.age < 18)).toBe(undefined)
 })
