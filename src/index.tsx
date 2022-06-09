d99c83878643730f6e967ee9fb1d29f56ea7f8ac
Ivan <paster-ivan@yandex.by>
2022-06-09 16:15:00 +0300

    HW8

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
+import store from './p2-homeworks/h10/bll/store'
 import * as serviceWorker from './serviceWorker'
 
 // igg2
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 )
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
-const initState = {
+export type LoadingActionType = ReturnType<typeof loadingAC>
 
+export type initStateType = typeof initState
+
+const initState = {
+    startLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: initStateType = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'LOADING': {
+            return {...state, startLoading:action.startLoading}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (startLoading: boolean) => {
+    return {
+        type: 'LOADING', startLoading
+    } as const
+} // fix any
